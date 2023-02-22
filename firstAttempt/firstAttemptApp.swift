3f4bb1f94e87ab11012b7b46e63cb468d0de3192
oliverwebp <119653688+oliverwebp@users.noreply.github.com>
2023-02-21 20:49:13 -0800

    Initial Commit

---
+++ firstAttempt/firstAttemptApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAttemptApp.swift
+//  firstAttempt
+//
+//  Created by Oliver Nguyen on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct firstAttemptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
