38336202d0234bc71ce5a58f5579e91af0b8a527
oliverwebp <119653688+oliverwebp@users.noreply.github.com>
2023-02-23 22:03:37 -0800

    Timer and Shell of App

--- firstAttempt/ContentView.swift
+++ firstAttempt/ContentView.swift
@@ -6,18 +6,102 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    @State var textColor = Color.white;
+    @State var longPressed = false;
+    @ObservedObject var stopwatch = Stopwatch()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Button("Settings", action: {
+                    
+                })
+                Spacer()
+                Button("Example", action: {
+                    
+                })
+                Spacer()
+                Text("Test")
+                    .onLongPressGesture(perform: {
+                        print("hi")
+                    })
+            }
+            Spacer()
+            ZStack {
+                Image("chonksoncat")
+                    .resizable()
+                    .ignoresSafeArea()
+                switch stopwatch.stateOfWatch {
+                case .stopped :
+                    Image("chonksoncat")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .onLongPressGesture(
+                            minimumDuration:99999,
+                            perform: {
+                            
+                        }, onPressingChanged: {_ in
+                            if self.longPressed {
+                                stopwatch.start()
+                                longPressed = false;
+                                textColor = Color.white
+                            } else {
+                                if textColor == Color.white {
+                                    textColor = Color.red
+                                } else {
+                                    textColor = Color.white
+                                }
+                            }
+                        })
+                        .simultaneousGesture(
+                            LongPressGesture(minimumDuration: 0.5)
+                                .onEnded({_ in
+                                textColor = Color.green
+                                self.longPressed = true;
+                            })
+                        )
+                case .running :
+                    Button(action: {
+                        stopwatch.stop()
+                        textColor = Color.white
+                    }, label: {
+                        Image("chonksoncat")
+                            .resizable()
+                            .ignoresSafeArea()
+                    })
+                }
+                Text(String(format:"%.4f",stopwatch.progressTime))
+                    .foregroundColor(textColor)
+            }
         }
-        .padding()
     }
 }
+enum stateofWatch {
+    case running
+    case stopped
+}
+
+class Stopwatch:ObservableObject {
+    @Published var progressTime = 0.0
+    @Published var stateOfWatch:stateofWatch = .stopped
+   var timer = Timer()
+
+    func start() {
+        stateOfWatch = .running
+        progressTime = 0.0;
+        timer = Timer.scheduledTimer(withTimeInterval: 0.001, repeats: true) {_ in
+            self.progressTime += 0.001
+            
+        }
+    }
+    func stop() {
+        stateOfWatch = .stopped
+        timer.invalidate()
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
