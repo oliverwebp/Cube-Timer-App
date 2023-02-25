228da47bb5747a35e0c299d0224d877053672d48
oliverwebp <119653688+oliverwebp@users.noreply.github.com>
2023-02-25 03:38:26 -0800

    Timer Formatting hh/mm/ss/mmm

--- CubeTimer/ContentView.swift
+++ CubeTimer/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
                     })
             }
             Spacer()
+            
             ZStack {
                 Image("chonksoncat")
                     .resizable()
@@ -47,16 +48,14 @@ struct ContentView: View {
                                 stopwatch.start()
                                 longPressed = false;
                                 textColor = Color.white
+                            } else if textColor == Color.white {
+                                textColor = Color.red
                             } else {
-                                if textColor == Color.white {
-                                    textColor = Color.red
-                                } else {
-                                    textColor = Color.white
-                                }
+                                textColor = Color.white
                             }
                         })
                         .simultaneousGesture(
-                            LongPressGesture(minimumDuration: 0.5)
+                            LongPressGesture(minimumDuration: 0.4)
                                 .onEnded({_ in
                                 textColor = Color.green
                                 self.longPressed = true;
@@ -72,8 +71,8 @@ struct ContentView: View {
                             .ignoresSafeArea()
                     })
                 }
-                Text(String(format:"%.4f",stopwatch.progressTime))
-                    .foregroundColor(textColor)
+                Text(stopwatch.timeInString)
+                    .foregroundColor(textColor).font(.system(size: 30, design: .monospaced))
             }
         }
     }
@@ -85,17 +84,53 @@ enum stateofWatch {
 
 class Stopwatch:ObservableObject {
     @Published var progressTime = 0.0
+    @Published var timeInString = "0.000"
     @Published var stateOfWatch:stateofWatch = .stopped
    var timer = Timer()
 
     func start() {
         stateOfWatch = .running
         progressTime = 0.0;
+        timeInString = "0.000"
         timer = Timer.scheduledTimer(withTimeInterval: 0.001, repeats: true) {_ in
-            self.progressTime += 0.001
             
+            self.progressTime += 0.001
+            self.display()
         }
     }
+    var hours: Int {
+       Int(progressTime) / 3600
+    }
+
+    var minutes: Int {
+      (Int(progressTime) % 3600) / 60
+    }
+
+    var seconds: Int {
+        Int(progressTime) % 60
+    }
+    var milli: Int {
+        Int(abs(Double(Int(progressTime)) - progressTime)*1000)
+    }
+    func display() {
+        
+        if (hours == 0 && minutes == 0) {
+            timeInString=String(seconds)
+        } else if (hours == 0 && minutes != 0) {
+            timeInString = String(minutes)
+            timeInString+=":"
+            timeInString+=String(format: "%02d", seconds)
+        } else {
+            timeInString = String(hours)
+            timeInString+=":"
+            timeInString+=String(format: "%02d", minutes)
+            timeInString+=":"
+            timeInString+=String(format: "%02d", seconds)
+        }
+        timeInString+="."
+        timeInString+=String(format:"%03d",milli)
+    }
+        
     func stop() {
         stateOfWatch = .stopped
         timer.invalidate()
